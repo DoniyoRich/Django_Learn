b55c695bfa9eb305bf972c180fa33e1dcc5a7733
Doniyor <donish1979@gmail.com>
2025-03-26 16:49:30 +0300

    initial commit for develop branch

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('show_data/', views.show_data, name='show_data'),
     path('submit_data/', views.submit_data, name='submit_data'),
